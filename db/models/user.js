d61a4b67db2f7a72c67ac6e1e565f8d2c5ea865c
keefe089e <kelleybkeefer@googlemail.com>
2022-04-30 13:22:27 -0500

    getallusers

--- db/models/user.js
+++ db/models/user.js
@@ -1,5 +1,6 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require("../client");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   // add your database adapter fns here
@@ -8,4 +9,16 @@ module.exports = {
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `SELECT * From users 
+    RETURNING *;
+  `
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
