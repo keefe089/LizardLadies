58cb0add06b0a45edc927f4413f3c38ed1ea89bc
keefe089e <kelleybkeefer@googlemail.com>
2022-04-30 14:02:30 -0500

    usersRouter/register

--- api/user.js
+++ api/user.js
@@ -16,38 +16,6 @@ usersRouter.use((req, res, next) => {
 
   next();
 });
-usersRouter.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    next({
-      name: "MissingCredentialsError",
-      message: "Please enter a username and password",
-    });
-  }
-
-  try {
-    const user = await getUserByUsername(username);
-
-    if (user && user.password == password) {
-      const token = jwt.sign(
-        {
-          id: user.id,
-          username: username,
-        },
-        process.env.JWT_SECRET
-      );
-    } else {
-      next({
-        name: "IncorrectCredentialsError",
-        message: "Username or password is incorrect",
-      });
-    }
-  } catch (error) {
-    console.log(error, "error in login");
-    next(error);
-  }
-});
-
 usersRouter.post("/register", async (req, res, next) => {
   const { username, password, email } = req.body;
   try {
@@ -80,62 +48,9 @@ usersRouter.post("/register", async (req, res, next) => {
       } else {
         next({
           name: "UserCreationError",
-          message: "There was a problem registering you. Please try again.",
+          message: "There was a problem with registration. Please try again.",
         });
       }
-    }
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
-});
-
-usersRouter.delete("/:userId", requireUser, async (req, res, next) => {
-  try {
-    const user = await getUserById(req.params.userId);
-
-    if (user && user.id === req.user.id) {
-      const deactivatedUser = await updateUser(user.id, { active: false });
-
-      res.send({ user: deactivatedUser });
-    } else {
-      next(
-        post
-          ? {
-              name: "UnauthorizedUserError",
-              message: "You cannot delete a user which is not yours",
-            }
-          : {
-              name: "UserNotFoundError",
-              message: "That user does not exist",
-            }
-      );
-    }
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
-});
-
-usersRouter.patch("/:userId", requireUser, async (req, res, next) => {
-  try {
-    const user = await getUserById(req.params.userId);
-
-    if (user && user.id === req.user.id) {
-      const reactivatedUser = await updateUser(user.id, { active: true });
-
-      res.send({ user: reactivatedUser });
-    } else {
-      // if there is a user logged in, throw UnauthorizedUserError, otherwise throw UserNotFoundError
-      next(
-        post
-          ? {
-              name: "UnauthorizedUserError",
-              message: "You cannot reactivate a user which is not yours",
-            }
-          : {
-              name: "UserNotFoundError",
-              message: "That user does not exist",
-            }
-      );
     }
   } catch ({ name, message }) {
     next({ name, message });
