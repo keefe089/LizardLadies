7316098ff6433fd99d1a761db6729ffa88ab54a7
keefe089e <kelleybkeefer@googlemail.com>
2022-04-30 16:58:21 -0500

    finished users

--- api/user.js
+++ api/user.js
@@ -57,4 +57,93 @@ usersRouter.post("/register", async (req, res, next) => {
   }
 });
 
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username: username,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.send({ message: "you're logged in!", token: token });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error, "error in login");
+    next(error);
+  }
+});
+
+usersRouter.delete("/:userId", requireUser, async (req, res, next) => {
+  try {
+    const user = await getUserById(req.params.userId);
+
+    if (user && user.id === req.user.id) {
+      const deactivatedUser = await updateUser(user.id, { active: false });
+
+      res.send({ user: deactivatedUser });
+    } else {
+      next(
+        post
+          ? {
+              name: "UnauthorizedUserError",
+              message: "You cannot delete a user which is not yours",
+            }
+          : {
+              name: "UserNotFoundError",
+              message: "That user does not exist",
+            }
+      );
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+usersRouter.patch("/:userId", requireUser, async (req, res, next) => {
+  try {
+    const user = await getUserById(req.params.userId);
+
+    if (user && user.id === req.user.id) {
+      const reactivatedUser = await updateUser(user.id, { active: true });
+
+      res.send({ user: reactivatedUser });
+    } else {
+      // if there is a user logged in, throw UnauthorizedUserError, otherwise throw UserNotFoundError
+      next(
+        post
+          ? {
+              name: "UnauthorizedUserError",
+              message: "You cannot reactivate a user which is not yours",
+            }
+          : {
+              name: "UserNotFoundError",
+              message: "That user does not exist",
+            }
+      );
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 module.exports = usersRouter;
--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,5 @@
 // grab our db client connection to use with our adapters
 const client = require("../client");
-const bcrypt = require("bcrypt");
-
-module.exports = {
-  // add your database adapter fns here
-  getAllUsers,
-};
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
@@ -22,3 +16,116 @@ async function getAllUsers() {
     throw error;
   }
 }
+async function createUser({ username, password }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+          INSERT INTO users(username, password) 
+          VALUES($1, $2) 
+          ON CONFLICT (username) DO NOTHING 
+          RETURNING username, id ;
+        `,
+      [username, password]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT * FROM users WHERE username=$1;
+      `,
+      [username]
+    );
+    if (!user) {
+      return null;
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      ` SELECT * 
+        FROM users
+        WHERE id = $1;
+        `,
+      [userId]
+    );
+
+    if (!user) {
+      return null;
+    }
+
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUser(params) {
+  try {
+    const { rows } = await client.query(
+      `SELECT id, username, password 
+          FROM users
+          WHERE username = $1
+        `,
+      [params.username]
+    );
+    const user = rows[0];
+    if (user.password == params.password) {
+      delete user.password;
+      return user;
+    }
+    return false;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      ` SELECT * 
+        FROM users
+        WHERE id = $1;
+        `,
+      [userId]
+    );
+
+    if (!user) {
+      return null;
+    }
+
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  // add your database adapter fns here
+  getAllUsers,
+  createUser,
+  getUserById,
+  getUserByUsername,
+  getUser,
+};
